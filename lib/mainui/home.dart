87fac49f6a2f23772bdfcf2df1212769815b7034
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-03-13 23:18:07 +0700

    menambah kelas HomePage untuk tampilan utama

---
+++ lib/mainui/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  final Map<String, dynamic>? recentOrder;
+
+  const HomePage({super.key, this.recentOrder});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
