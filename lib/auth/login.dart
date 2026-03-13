cddc96afb80b63cdce68dedce49045694436c058
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-03-13 22:55:26 +0700

    menambah tampilan halaman login dengan penanganan kesalahan gambar

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_route_parsing/main_layout.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -19,5 +20,55 @@ class _LoginPageState extends State<LoginPage> {
     super.dispose();
   }
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'Login',
+      showAppBar: false,
+      child: Container(
+        color: MainLayout.backgroundColor,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32.0,
+                vertical: 24.0,
+              ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Image.asset(
+                      'assets/images/login.png',
+                      height: 200,
+                      fit: BoxFit.contain,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            color: MainLayout.primaryColor.withOpacity(0.1),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Center(
+                            child: Icon(
+                              Icons.image_not_supported_outlined,
+                              size: 80,
+                              color: MainLayout.primaryColor,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    const SizedBox(height: 40),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+    );
+  }
 }
